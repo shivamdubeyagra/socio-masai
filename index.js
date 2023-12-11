bd3a610e51c5137f2feb496dc2a56d1a4aa0e746
shivamdubeyagra <shivamdubeyagra@gmail.com>
2023-12-11 17:00:05 +0530

    cors added

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const { connection } = require("./db");
 const { userRouter } = require("./routes/user.routes");
 const { postRouter } = require("./routes/post.routes");
 // const { auth } = require("./middleware/auth.middleware");
+const cors = require('cors')
 require("dotenv").config();
 const app = express();
+app.use(cors())
 app.use(express.json());
+
 app.use("/users",userRouter);
 app.use("/posts",postRouter);
 
